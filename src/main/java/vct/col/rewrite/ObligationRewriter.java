87a0a3c632f876374796bc1991c34795f5a1beb3
Matthiti <mj.roelink@outlook.com>
2020-05-27 13:05:34 +0200

    Create enoughObs method

--- src/main/java/vct/col/rewrite/ObligationRewriter.java
+++ src/main/java/vct/col/rewrite/ObligationRewriter.java
@@ -2,13 +2,18 @@ package vct.col.rewrite;
 
 import hre.ast.MessageOrigin;
 import vct.col.ast.expr.MethodInvokation;
+import vct.col.ast.expr.StandardOperator;
 import vct.col.ast.stmt.decl.*;
+import vct.col.ast.stmt.terminal.ReturnStatement;
+import vct.col.ast.type.PrimitiveSort;
+import vct.col.ast.util.ContractBuilder;
 
 import java.util.ArrayList;
 
 public class ObligationRewriter extends AbstractRewriter {
 
   private static final String OBLIGATION_VARIABLE = "obsVars";
+  private static final String ENOUGH_OBS_METHOD = "enoughObs";
 
   private ASTClass obligationClass;
 
@@ -18,9 +23,16 @@ public class ObligationRewriter extends AbstractRewriter {
 
   @Override
   public void visit(ASTSpecial s) {
-    if (s.kind != ASTSpecial.Kind.Wait) {
-      super.visit(s);
-      return;
+    switch (s.kind) {
+      case Wait:
+        int n = 0;
+        break;
+      case Notify:
+        break;
+      case NotifyAll:
+        break;
+      default:
+        super.visit(s);
     }
   }
 
@@ -52,11 +64,48 @@ public class ObligationRewriter extends AbstractRewriter {
 
   @Override
   public ProgramUnit rewriteAll() {
+    create.setOrigin(new MessageOrigin("Generated code: Obligation variables"));
     obligationClass = create.ast_class("ObligationVariables", ASTClass.ClassKind.Plain, null, null, null);
-    obligationClass.setOrigin(new MessageOrigin("Generated code: Obligation variables"));
-//    obligationClass.add(create.field_decl("Wt", create.primitive_type(PrimitiveSort.Integer), create.constant(42)));
     ProgramUnit res = super.rewriteOrdered();
+
+    for (DeclarationStatement var : variables()) {
+      obligationClass.add(var);
+    }
+    obligationClass.add_dynamic(enoughObsMethod());
+
     res.add(obligationClass);
     return res;
+  }
+
+  private Method enoughObsMethod() {
+    ContractBuilder cb = new ContractBuilder();
+    cb.requires(create.expression(StandardOperator.GTE, create.local_name("Wt_v"), create.constant(0)));
+    cb.requires(create.expression(StandardOperator.GTE, create.local_name("Ot_v"), create.constant(0)));
+
+    DeclarationStatement[] args = new DeclarationStatement[] {
+        create.field_decl("Wt_v", create.primitive_type(PrimitiveSort.Integer)),
+        create.field_decl("Ot_v", create.primitive_type(PrimitiveSort.Integer))
+    };
+
+    ReturnStatement stat = create.return_statement(
+      create.expression(StandardOperator.Or,
+          create.expression(StandardOperator.EQ, create.local_name("Wt_v"), create.constant(0)),
+          create.expression(StandardOperator.GT, create.local_name("Ot_v"), create.constant(0))
+      )
+    );
+
+    return create.method_decl(
+        create.primitive_type(PrimitiveSort.Boolean),
+        cb.getContract(),
+        ENOUGH_OBS_METHOD,
+        args,
+        create.block(stat)
+    );
+  }
+
+  private DeclarationStatement[] variables() {
+    return new DeclarationStatement[] {
+        create.field_decl("Wt", create.primitive_type(PrimitiveSort.Integer), create.constant(42))
+    };
   }
 }
